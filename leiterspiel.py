f5073a44318c6a3e5aefa493d1bc474c46f16f30
sn1l <50239705+sn1l@users.noreply.github.com>
2019-07-05 12:24:17 +0200

    Update leiterspiel.py

--- leiterspiel.py
+++ leiterspiel.py
@@ -1,16 +1,16 @@
-﻿import pre_file
+﻿import pri_file
 def gldbchscnjctr():
 	eingabe = input("Welche Zahl möchtest du als Summe zweier Primzahlen ausgegeben bekommen?\n")
 	eingabe = int(eingabe)
-	if eingabe in primes.plist():
+	if eingabe in Primes.plist():
     		print("Die eingebenene Zahl ist bereits eine Primzahl")
-	elif eingabe not in primes.plist():
-    		for h in range(len(primes.plist())):
-            		if eingabe < primes.plist()[h]:
-                		pO = primes.plist()[h-1]
-    		for pU in primes.plist():
-        		for h in range(len(primes.plist())):
-            			if eingabe < primes.plist()[h]:
-                			pO = primes.plist()[h-1]
+	elif eingabe not in Primes.plist():
+    		for h in range(len(Primes.plist())):
+            		if eingabe < Primes.plist()[h]:
+                		pO = Primes.plist()[h-1]
+    		for pU in Primes.plist():
+        		for h in range(len(Primes.plist())):
+            			if eingabe < Primes.plist()[h]:
+                			pO = Primes.plist()[h-1]
             			if pO+pU == eingabe:
                 			print(eingabe, " = ", pU, " + ",pO)
