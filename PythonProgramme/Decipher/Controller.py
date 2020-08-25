95a104739c4816e1f331befe388bcf6b2d6e1af8
1R3tr0 <niklaskoro@gmail.com>
2020-08-25 15:27:08 +0200

    Updates 24.08.20

--- PythonProgramme/Decipher/Controller.py
+++ PythonProgramme/Decipher/Controller.py
@@ -2,6 +2,7 @@ import tkinter
 from Decoder import Caesar_Cipher
 from Decoder import A1Z26_Cipher
 from Decoder import Atbash_Cipher
+from Decoder import Vigenere_Cipher
 
 class Controller:
     active=[]
@@ -12,11 +13,12 @@ class Controller:
     mode="alphabet"
     def __init__(self,master):
         self.root=master
-        self.panel=tkinter.Frame(self.root,width=200,height=800,bd=5,bg="#000000")
-        self.properties=tkinter.Text(self.panel,bd=2,bg="#000000",fg="#FFFFFF",font=("Courier",10),insertbackground="#00FF31",state="disabled")
+        self.panel=tkinter.Frame(self.root,width=200,height=800,bd=5,bg="#262626")
+        self.properties=tkinter.Text(self.panel,bd=2,bg="#262626",fg="#FFFFFF",font=("Courier",10),insertbackground="#262626",state="disabled")
         self.caesar=Caesar_Cipher(self.root,self.language)
         self.atbash=Atbash_Cipher(self.root,self.language)
         self.a1z26=A1Z26_Cipher(self.root,self.language)
+        self.vigenere=Vigenere_Cipher(self.root, self.language)
         self.Menu=tkinter.Menu(self.root)
         #Menu Punkte
         self.MF1=tkinter.Menu(master=self.Menu)
@@ -25,6 +27,7 @@ class Controller:
         self.MF2.add_command(label="Caesar crypt",command=lambda x="CAESAR": self.Create(x))
         self.MF2.add_command(label="Atbash crypt",command=lambda x="ATBASH": self.Create(x))
         self.MF2.add_command(label="A1Z26 crypt",command=lambda x="A1Z26": self.Create(x))
+        self.MF2.add_command(label="Vigenère crypt", command=lambda x="VIGENERE": self.Create(x))
         self.MF1.add_command(label="beenden", command=self.root.destroy)
         
         self.root.geometry("800x800")
@@ -32,6 +35,7 @@ class Controller:
         self.Menu.add_cascade(label="Tools",menu=self.MF2)
         self.root["menu"]=self.Menu
         self.panel.place(x=600,y=0,anchor="nw")
+        self.root["bg"]="#262626"
         self.root.mainloop()
 
     def Create(self,mode):
@@ -45,6 +49,9 @@ class Controller:
             self.active[len(self.active)-1].build()
         elif mode=="A1Z26":
             self.active.append(self.a1z26)
-            self.active[len(self.active)-1].build()            
+            self.active[len(self.active)-1].build()   
+        elif mode=="VIGENERE":
+            self.active.append(self.vigenere)
+            self.active[len(self.active)-1].build()      
 tk=tkinter.Tk()
 Controller(tk)
--- PythonProgramme/Decipher/Decoder.py
+++ PythonProgramme/Decipher/Decoder.py
@@ -8,13 +8,13 @@ class Cipher:
     # Ganz einfach: Weil ich es ausprobieren wollte!
     
     def __init__(self,master,language):
-        self.frame=tkinter.Frame(master=master,width=600,height=200,relief="ridge",bg="#000000",bd=10)
-        self.lencrypted=tkinter.Label(self.frame,anchor="w",justify="left",bg="#000000",fg="#FFFFFF",font=("Courier",16),text="Encoded text:")#Controller.language[Controller.cur_language]["lencrypt"])
-        self.ldecrypted=tkinter.Label(self.frame,anchor="w",justify="left",bg="#000000",fg="#FFFFFF",font=("Courier",16),text="Decoded text:")#Controller.language[Controller.cur_language]["ldecrypt"])
-        self.encrypted=tkinter.Entry(self.frame,font=("Courier",16))
-        self.decrypted=tkinter.Entry(self.frame,font=("Courier",16))
-        self.execute=tkinter.Button(self.frame,bg="#000000",fg="#FFFFFF",font=("Courier",16),bd=4,text="decrypt",command=self.decrypt)
-        self.quit=tkinter.Button(self.frame,bg="#000000",fg="#FFFFFF",activebackground="#FF0000",font=("Courier",16),width=20,height=20,text="X",command=self.frame.place_forget)
+        self.frame=tkinter.Frame(master=master,width=600,height=200,relief="ridge",bg="#262626",bd=10)
+        self.lencrypted=tkinter.Label(self.frame,anchor="w",justify="left",bg="#262626",fg="#FFFFFF",font=("Courier",16),text="Encoded text:")#Controller.language[Controller.cur_language]["lencrypt"])
+        self.ldecrypted=tkinter.Label(self.frame,anchor="w",justify="left",bg="#262626",fg="#FFFFFF",font=("Courier",16),text="Decoded text:")#Controller.language[Controller.cur_language]["ldecrypt"])
+        self.encrypted=tkinter.Entry(self.frame,font=("Courier",16), bg="#262626", fg="#FFFFFF")
+        self.decrypted=tkinter.Entry(self.frame,font=("Courier",16), bg="#262626", fg="#FFFFFF")
+        self.execute=tkinter.Button(self.frame,bg="#262626",fg="#FFFFFF",font=("Courier",16),bd=4,text="decrypt",command=self.decrypt)
+        self.quit=tkinter.Button(self.frame,bg="#262626",fg="#FFFFFF",font=("Courier",16),width=20,height=20,text="X",command=self.frame.place_forget)
         
     def build(self):
         self.frame.place(x=0,y=0,anchor="nw")
@@ -35,8 +35,8 @@ class Caesar_Cipher(Cipher):
 
     def __init__(self,master,language):
         Cipher.__init__(self,master,language)
-        self.lkey=tkinter.Label(self.frame,anchor="w",bg="#000000",fg="#FFFFFF",font=("Courier",16),text="Key")#language[Controller.cur_language]["lkey"])
-        self.key=tkinter.Entry(self.frame,font=("Courier",16))
+        self.lkey=tkinter.Label(self.frame,anchor="w",bg="#262626",fg="#FFFFFF",font=("Courier",16),text="Key")#language[Controller.cur_language]["lkey"])
+        self.key=tkinter.Entry(self.frame,font=("Courier",16), bg="#262626", fg="#FFFFFF")
     
     def build(self):
         self.frame.place(x=0,y=0,anchor="nw")
@@ -96,7 +96,7 @@ class A1Z26_Cipher(Cipher):
         Cipher.__init__(self,master,language)
     
     def build(self):
-        Cipher.build(self)
+       Cipher.build()
     
     def decrypt(self):
         result=""
@@ -119,3 +119,82 @@ class A1Z26_Cipher(Cipher):
             pass
         self.decrypted.delete(0,"end")
         self.decrypted.insert(0,result)
+
+class Vigenere_Cipher(Cipher):
+
+    def __init__(self, master, language):
+        super().__init__(master, language)
+        self.lkey=tkinter.Label(self.frame,anchor="w",bg="#262626",fg="#FFFFFF",font=("Courier",16),text="Key")#language[Controller.cur_language]["lkey"])
+        self.key=tkinter.Entry(self.frame,font=("Courier",16), bg="#262626", fg="#FFFFFF")
+
+    def build(self):
+        self.frame.place(x=0,y=0,anchor="nw")
+        self.lencrypted.place(x=0,y=10,anchor="nw",width=200,height=20)
+        self.ldecrypted.place(x=0,y=90,anchor="nw",width=200,height=20)
+        self.encrypted.place(x=201,y=10,anchor="nw",width=330,height=20)
+        self.decrypted.place(x=201,y=90,anchor="nw",width=330,height=20)
+        self.execute.place(x=0,y=130,anchor="nw",width=580,height=50)
+        self.quit.place(x=560,y=0,anchor="nw",width=20,height=20)
+        self.lkey.place(x=0,y=50,anchor="nw",width=200,height=20)
+        self.key.place(x=201,y=50,anchor="nw",width=330,height=20)  
+        
+    def decrypt(self):
+        en_text = self.encrypted.get()
+        de_text = self.decrypted.get()
+        key = self.key.get()
+        if de_text != "" and en_text != "":
+            self.encrypted.insert(0, "Error u can't enter both")
+            self.decrypted.insert(0, "") 
+        elif de_text == "" and en_text != "":
+            self.decrypted.insert(0, self.decode(en_text, key))
+        elif en_text == "" and de_text != "":
+            self.encrypted.insert(0, self.encode(de_text, key))
+        else:
+            self.encrypted.insert(0, "unexpected Error")
+            self.decrypted.insert(0, "") 
+
+    def decode(self, code: str, key: str) -> str :
+        result = ""
+        lenght = len(code)
+        extra_letters = ["!", ".", ",", ":", ";"]
+        i = 0
+
+        for _ in extra_letters:
+            code = code.replace(_, "")
+        code = code.upper()
+        key = key.upper()
+        while i < lenght:
+            for _ in key:
+                if i < lenght:
+                    tmp_key = ord(_) - 65
+                    tmp_result = ord(code[i]) - tmp_key
+                    if (tmp_result < 65):
+                        tmp_result += 26
+                    result += chr(tmp_result)
+                else:
+                    break
+                i+=1
+        return result
+
+    def encode(self, plain_text: str, key: str) -> str :
+        result = ""
+        lenght = len(plain_text)
+        extra_letters = ["!", ".", ",", ":", ";"]
+        i = 0
+
+        for _ in extra_letters:
+            plain_text = plain_text.replace(_, "")
+        plain_text = plain_text.upper()
+        key = key.upper()
+        while i < lenght:
+            for _ in key:
+                if i < lenght:
+                    tmp_key = ord(_) - 65
+                    tmp_result = ord(plain_text[i]) + tmp_key
+                    if (tmp_result > 91):
+                        tmp_result -= 26
+                    result += chr(tmp_result)
+                else:
+                    break
+                i+=1
+        return result
